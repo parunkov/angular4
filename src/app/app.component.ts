afa880e5533dbbfa6db88f1e948610a3dbb9089d
parunkov <parunkov@gmail.com>
2024-03-28 16:33:28 +0500

    filter by text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
+
+export interface Post {
+  title: string;
+  text: string;
+  [index: string]: any;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, FilterPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent  {
+
+  search = '';
+  searchField = 'title';
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+    {title: 'Bred', text: 'The best bread in the world'},
+    {title: 'Javascript', text: 'The best language in the world'},
+  ];
 }
