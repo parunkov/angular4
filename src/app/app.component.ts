f42f76d762c908e38376fbf23356d554d019ed55
parunkov <parunkov@gmail.com>
2024-03-28 15:58:31 +0500

    begin filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MultByPipe } from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MultByPipe, ExMarksPipe],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent  {
-  e: number = Math.E;
-  str = 'hello world';
-  date: Date = new Date();
-  float: number = 0.42;
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4
-      }
-    }
-  }
 }
