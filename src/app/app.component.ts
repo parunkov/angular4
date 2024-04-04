db6015e5a4c6f578e290d1697a604d1cf99a1200
parunkov <parunkov@gmail.com>
2024-04-04 15:51:24 +0500

    controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null)
+    })
+  }
+  submit() {
+    console.log('Form: ', this.form)
+    const formData = {...this.form.value}
+    console.log('formData', formData)
+  }
 }
