5dd1eca5a9be504e1c3565e4aa2efd2ddfe8355d
parunkov <parunkov@gmail.com>
2024-04-04 15:28:48 +0500

    forms begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppCounterService } from './services/app-counter.service';
-import { LocalCounterService } from './services/local-counter.service';
-import { CounterComponent } from './counter/counter.component';
-
-export interface Post {
-  title: string;
-  text: string;
-  [index: string]: any;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, CounterComponent],
-  providers: [LocalCounterService],
+  imports: [CommonModule, FormsModule],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  constructor(
-    public appCounterService: AppCounterService,
-    public localCounterService: LocalCounterService
-    ) {}
 }
