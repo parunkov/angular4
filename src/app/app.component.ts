80ee76cb188390b05a68f3a966a295f31ad41596
parunkov <parunkov@gmail.com>
2024-04-04 08:50:14 +0500

    services begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,24 +17,5 @@ export interface Post {
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolved')
-    }, 4000)
-  })
-
-  date$: Observable<Date> = new Observable(obs => {
-    setInterval(() => {
-      obs.next(new Date())
-    }, 1000)
-  })
-
-  date: Date
-
-  ngOnInit(): void {
-    this.date$.subscribe(date => {
-      this.date = date
-    })
-  }
+export class AppComponent {
 }
