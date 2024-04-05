74d3d5b6a51d544aeed5b06cd13dcb9e4c26fd90
parunkov <parunkov@gmail.com>
2024-04-05 14:11:33 +0500

    error messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +12,18 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 })
 
 export class AppComponent implements OnInit {
-  form: FormGroup 
+  form: FormGroup
   ngOnInit(): void {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(null)
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(12)])
     })
   }
   submit() {
-    console.log('Form: ', this.form)
-    const formData = {...this.form.value}
-    console.log('formData', formData)
+    if (this.form.valid) {
+      console.log('Form: ', this.form)
+      const formData = { ...this.form.value }
+      console.log('formData', formData)
+    }
   }
 }
