3d407d2df056a61641c3d159d8cdc24c28a2c255
parunkov <parunkov@gmail.com>
2024-04-10 15:12:54 +0500

    ng models begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MyValidators } from '../my.vaalidators';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,46 +11,5 @@ import { MyValidators } from '../my.vaalidators';
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
-  form: FormGroup
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl('', [Validators.email, Validators.required, MyValidators.restrictedEmails], [MyValidators.uniqEmail] as any),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      address: new FormGroup({
-        country: new FormControl('by'),
-        city: new FormControl('', Validators.required)
-      }),
-      skills: new FormArray([])
-    })
-  }
-  submit() {
-    if (this.form.valid) {
-      console.log('Form: ', this.form)
-      const formData = { ...this.form.value }
-      console.log('formData', formData)
-      this.form.reset()
-    }
-  }
-
-  setCapital() {
-    const cityMap: {[index: string]:string}  = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск'
-    };
-    const cityKey = this.form.get('address')?.get('country')?.value;
-    const city: string = cityMap[cityKey];
-    this.form.patchValue({address: {city}});
-  }
-
-  addSkill() {
-    const control = new FormControl('', Validators.required);
-    // (<FormArray>this.form.get('skills')).push(control);
-    (this.form.get('skills') as FormArray).push(control);
-  }
-
-  getControls() {
-    return (this.form.get('skills') as FormArray).controls;
-  }
+export class AppComponent {
 }
