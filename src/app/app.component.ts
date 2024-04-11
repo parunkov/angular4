2f4f08d05a988b84f6b3469e3308afaa7e1c4598
parunkov <parunkov@gmail.com>
2024-04-11 14:21:00 +0500

    http client begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SwitchComponent } from './switch/switch.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, SwitchComponent],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
-  appState = 'on'
-  handleChange() {
-    console.log(this.appState);
-    
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(todos => {
+      console.log(todos);
+      this.todos = todos;
+    })
   }
 }
