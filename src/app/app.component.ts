0858343aba365e76c84698a1256bcf2ad7a78487
parunkov <parunkov@gmail.com>
2024-04-22 09:27:38 +0500

    loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,4 +55,11 @@ export class AppComponent implements OnInit {
       this.loading = false;
     })
   }
+  
+  removeTodo(id: number | undefined) {
+    this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .subscribe(() => {
+        this.todos = this.todos.filter(t => t.id !== id);
+      })
+  }
 }
