30d3b9212e806568c50f49de2cc7e0a0e699c2d0
parunkov <parunkov@gmail.com>
2024-04-22 09:15:59 +0500

    loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { delay } from 'rxjs';
 
 export interface Todo {
   completed: boolean;
@@ -21,12 +22,37 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   todos: Todo[] = [];
+  todoTitle = '';
+  loading = false;
 
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(todos => {
-      console.log(todos);
+    this.fetchTodos();
+  }
+
+  addTodo () {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+    
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe(todo => {
+      console.log(todo);
+      this.todos.push(todo);
+      this.todoTitle = '';
+    })
+  }
+
+  fetchTodos() {
+    this.loading = true;
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(1500))
+      .subscribe(todos => {
       this.todos = todos;
+      this.loading = false;
     })
   }
 }
