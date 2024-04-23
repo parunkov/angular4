821802e80d7d082f2f04bf74e66509fa16db9924
parunkov <parunkov@gmail.com>
2024-04-23 12:49:25 +0500

    pages begin

---
+++ src/app/about-extra/about-extra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-extra',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-extra.component.html',
+  styleUrl: './about-extra.component.scss'
+})
+export class AboutExtraComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Todo, TodosService } from './services/todos.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule, HomeComponent],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
-
-  todos: Todo[] = [];
-  todoTitle = '';
-  loading = false;
-  error = '';
-
-  constructor(private todosService: TodosService) { }
-  ngOnInit(): void {
-    this.fetchTodos();
-  }
-
-  addTodo() {
-    if (!this.todoTitle.trim()) {
-      return;
-    }
-
-    this.todosService.addTodo({
-      title: this.todoTitle,
-      completed: false
-    }).subscribe(todo => {
-      console.log(todo);
-      this.todos.push(todo);
-      this.todoTitle = '';
-    })
-  }
-
-  fetchTodos() {
-    this.loading = true;
-    this.todosService.fetchTodos()
-      .subscribe(todos => {
-        if (todos) this.todos = todos;
-        this.loading = false;
-      }, error => {
-        this.error = error.message;
-        
-      })
-  }
-
-  removeTodo(id: number | undefined) {
-    this.todosService.removeTodo(id)
-      .subscribe(() => {
-        this.todos = this.todos.filter(t => t.id !== id);
-      })
-  }
-
-  completeTodo(id: number | undefined) {
-    this.todosService.completeTodo(id).subscribe(todo => {
-      const item = this.todos.find(t => t.id === todo.id);
-      if (item) item.completed = true;
-    })
-  }
+export class AppComponent {
 }
