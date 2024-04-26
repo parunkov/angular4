5285d1f5e0e704dfc4f4215f2c965449770ade48
parunkov <parunkov@gmail.com>
2024-04-26 10:41:54 +0500

    error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
 import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -11,5 +12,7 @@ export const routes: Routes = [
         {path: 'extra', component: AboutExtraComponent}
     ]},
     {path: 'posts', component: PostsComponent},
-    {path: 'posts/:id', component: PostComponent}
+    {path: 'posts/:id', component: PostComponent},
+    {path: 'error', component: ErrorPageComponent},
+    {path: '**', redirectTo: '/error'}
 ];
